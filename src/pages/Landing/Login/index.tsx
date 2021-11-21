23086265e1b6723bc2882e713713657ba860d615
CarinaRoberta <silva.carina@live.com>
2021-11-21 12:50:42 -0300

    Please enter the commit message for your changes. Lines starting correções

--- src/pages/Landing/Login/index.tsx
+++ src/pages/Landing/Login/index.tsx
@@ -1,21 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "./style";
 import logoImage from "../../../assets/logo.jpg";
-import Button from "../../../components/Button";
 import { Link } from "react-router-dom";
 import SignUp from "../SignUp";
 
 const Login: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <Container>
       <img src={logoImage} alt="Saúde Fácil" />
       <h1>Faça seu login</h1>
       <form>
         <h3>E-mail</h3>
-        <input type="email" />
+        <input type="email" onChange={(e) => setEmail(e.target.value)} />
         <h3>Senha</h3>
-        <input type="password" />
-        <Button>Entrar</Button>
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+        <Link to="/home">Entrar</Link>
         <Link to="/cadastro">Cadastre-se</Link>
       </form>
     </Container>
